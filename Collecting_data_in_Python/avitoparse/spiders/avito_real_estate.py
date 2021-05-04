66aadba2272e880d00296f09027353b1852a4509
Aleksey Kuritsyn <bimastics@yandex.com>
2021-05-04 22:18:43 +0700

    Implemented getting apartment pages

--- Collecting_data_in_Python/avitoparse/spiders/avito_real_estate.py
+++ Collecting_data_in_Python/avitoparse/spiders/avito_real_estate.py
@@ -6,10 +6,18 @@ class AvitoRealEstateSpider(scrapy.Spider):
     allowed_domains = ['www.avito.ru', 'avito.ru']
     start_urls = ['https://www.avito.ru/abakan/kvartiry/prodam-ASgBAgICAUSSA8YQ?cd=1']
 
-    def parse(self, response):
+    def parse(self, response):  # Page pagination
         for num in response.xpath('//div[@data-marker="pagination-button"]//span/text()'):
             try:
                 tmp = int(num.get())
-                yield response.follow(f'https://www.avito.ru/abakan/kvartiry/prodam-ASgBAgICAUSSA8YQ?cd=1&p={tmp}', callback=self.parse)
+                yield response.follow(f'https://www.avito.ru/abakan/kvartiry/prodam-ASgBAgICAUSSA8YQ?cd=1&p={tmp}',
+                                      callback=self.parse)
             except ValueError as e:
                 continue
+
+        # Getting links of apartments
+        for abs_url in response.xpath('//div[@data-marker="item"]//a//@href'):
+            yield response.follow(abs_url, callback=self.abs_parse)
+
+    def abs_parse(self, response):
+        print(1)
