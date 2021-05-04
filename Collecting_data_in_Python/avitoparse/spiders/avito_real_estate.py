f0da727f0f8bf7e98d63040201c7c2d154b202f9
Aleksey Kuritsyn <bimastics@yandex.com>
2021-05-04 21:48:21 +0700

    Implemented transition by pagination

--- Collecting_data_in_Python/avitoparse/spiders/avito_real_estate.py
+++ Collecting_data_in_Python/avitoparse/spiders/avito_real_estate.py
@@ -3,8 +3,13 @@ import scrapy
 
 class AvitoRealEstateSpider(scrapy.Spider):
     name = 'avito_real_estate'
-    allowed_domains = ['avito.ru']
-    start_urls = ['http://avito.ru/']
+    allowed_domains = ['www.avito.ru', 'avito.ru']
+    start_urls = ['https://www.avito.ru/abakan/kvartiry/prodam-ASgBAgICAUSSA8YQ?cd=1']
 
     def parse(self, response):
-        pass
+        for num in response.xpath('//div[@data-marker="pagination-button"]//span/text()'):
+            try:
+                tmp = int(num.get())
+                yield response.follow(f'https://www.avito.ru/abakan/kvartiry/prodam-ASgBAgICAUSSA8YQ?cd=1&p={tmp}', callback=self.parse)
+            except ValueError as e:
+                continue
