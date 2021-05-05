faee743f4b74ba2fe96ac089637236e8b5d6b3f7
Aleksey Kuritsyn <bimastics@yandex.com>
2021-05-06 00:59:58 +0700

    Created Pipeline to generate CSV file
    This requires a databases.csv file since the code assumes that the file already exists

--- Collecting_data_in_Python/avitoparse/pipelines.py
+++ Collecting_data_in_Python/avitoparse/pipelines.py
@@ -7,16 +7,19 @@
 # useful for handling different item types with a single interface
 # from itemadapter import ItemAdapter
 import scrapy
+import csv
 from pymongo import MongoClient
 from scrapy.pipelines.images import ImagesPipeline
 
 
 class AvitoparsePipeline:
     def __init__(self):
-        client = MongoClient()
-        db = client.test
+        client = MongoClient('localhost', 27017)
+        self.mongo_base = client.avito_photo_312
 
     def process_item(self, item, spider):
+        collection = self.mongo_base[spider.name]
+        collection.insert_one(item)
         return item
 
 
@@ -34,3 +37,25 @@ class ImgPipeline(ImagesPipeline):
         if results:
             item['photos'] = [itm[1] for itm in results]
         return item
+
+
+class CSVPipeline(object):
+    def __init__(self):
+        self.file = f'database.csv'
+        with open(self.file, 'r', newline='') as csv_file:
+            self.tmp_data = csv.DictReader(csv_file).fieldnames
+
+        self.csv_file = open(self.file, 'a', newline='', encoding='UTF-8')
+
+    def process_item(self, item, spider):
+        colums = item.fields.keys()
+
+        data = csv.DictWriter(self.csv_file, colums)
+        if not self.tmp_data:
+            data.writeheader()
+            self.tmp_data = True
+        data.writerow(item)
+        return item
+
+    def __del__(self):
+        self.csv_file.close()
--- Collecting_data_in_Python/avitoparse/settings.py
+++ Collecting_data_in_Python/avitoparse/settings.py
@@ -29,7 +29,7 @@ IMAGES_STORE = 'images_avito'
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 1.5
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -67,8 +67,9 @@ TELNETCONSOLE_ENABLED = False
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'avitoparse.pipelines.AvitoparsePipeline': 300,
-   'avitoparse.pipelines.ImgPipeline': 100
+   # 'avitoparse.pipelines.AvitoparsePipeline': 400,
+   # 'avitoparse.pipelines.ImgPipeline': 300,
+   'avitoparse.pipelines.CSVPipeline': 100
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
