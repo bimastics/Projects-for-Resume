56ce034eb971424e9174cfb0476f6da5bff361dd
Aleksey Kuritsyn <bimastics@yandex.com>
2021-05-06 00:57:57 +0700

    Fixed getting apartment links. Added data fetching

--- Collecting_data_in_Python/avitoparse/items.py
+++ Collecting_data_in_Python/avitoparse/items.py
@@ -6,12 +6,27 @@
 import scrapy
 from scrapy.loader.processors import MapCompose, TakeFirst
 
+
 class AvitoparseItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
 
 
+
 class AvitoRealEstateItem(scrapy.Item):
-    _id = scrapy.Field()
+    id = scrapy.Field(input_processor=MapCompose(lambda data: int(data[2:])), output_processor=TakeFirst())
+    url = scrapy.Field(output_processor=TakeFirst())
+    title = scrapy.Field(output_processor=TakeFirst())
     photos = scrapy.Field()
+
+    house_type = scrapy.Field(output_processor=TakeFirst())
+    house_floor = scrapy.Field(output_processor=TakeFirst())
+    floor = scrapy.Field(output_processor=TakeFirst())
+    rooms = scrapy.Field(output_processor=TakeFirst())
+    total_square = scrapy.Field(output_processor=TakeFirst())
+    active_square = scrapy.Field(output_processor=TakeFirst())
+    kitchen_square = scrapy.Field(output_processor=TakeFirst())
+    balcony_or_loggia = scrapy.Field(output_processor=TakeFirst())
+    view_from_window = scrapy.Field(output_processor=TakeFirst())
+    Finishing = scrapy.Field(output_processor=TakeFirst())
--- Collecting_data_in_Python/avitoparse/spiders/avito_real_estate.py
+++ Collecting_data_in_Python/avitoparse/spiders/avito_real_estate.py
@@ -21,10 +21,39 @@ class AvitoRealEstateSpider(scrapy.Spider):
                 continue
 
         # Getting links of apartments
-        for abs_url in response.xpath('//div[@data-marker="item"]//a//@href'):
+        for abs_url in response.xpath('//div[@data-marker="item"]/div/div/a//@href'):
             yield response.follow(abs_url, callback=self.abs_parse)
 
     def abs_parse(self, response):
         item = ItemLoader(AvitoRealEstateItem(), response)
+        item.add_css('id', 'div.item-view-search-info-redesign span::text')
+        item.add_value('url', response.url)
+        item.add_css('title', 'div.title-info-main h1.title-info-title span::text')
         item.add_xpath('photos', "//div[contains(@class, 'gallery-img-frame')]/@data-url")
+
+        house_data = response.css("div.item-params ul li")
+
+        for param in house_data:
+            info = param.css("::text").extract()
+
+            if 'Тип дома' in info[1]:
+                item.add_value('house_type', info[-1])
+            elif 'Этаж' in info[1]:
+                item.add_value('floor', int(info[-1][0]))
+                item.add_value('house_floor', int(info[-1][-2]))
+            elif 'Количество комнат' in info[1]:
+                item.add_value('rooms', int(info[-1].strip()))
+            elif 'Общая площадь' in info[1]:
+                item.add_value('total_square', float(info[-1][:-4]))
+            elif 'Балкон' in info[1]:
+                item.add_value('balcony_or_loggia', info[-1].strip())
+            elif 'Вид из окна' in info[1]:
+                item.add_value('view_from_window', info[-1])
+            elif 'Жилая площадь' in info[1]:
+                item.add_value('active_square', float(info[-1][:-4]))
+            elif 'Площадь кухни' in info[1]:
+                item.add_value('kitchen_square', float(info[-1][:-4]))
+            elif 'Отделка' in info[1]:
+                item.add_value('Finishing', info[-1].strip())
+
         yield item.load_item()
